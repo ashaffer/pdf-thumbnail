c4e7dc9f0148ee142804bedcc2fc5c97b994c9b2
Andrew Shaffer <andrew@weo.io>
2015-01-20 17:16:33 -0800

    add streams

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 var gm = require('gm');
 
-module.exports = function(file, opts, cb) {
-  opts = opts || {};
-  var out = fs.createWriteStream(opts.out || 'out.png');
-
-  gm(file)
+module.exports = function(opts) {
+  return gm(this)
     .setFormat('png')
-    .write(fs.createWriteStream(out), function(err) {
-      cb && cb(err);
-    });
+    .write();
 };
