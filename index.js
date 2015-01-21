749c4f4ed1b8348d27ad5b998ef3c84eeafaa125
Andrew Shaffer <andrew@weo.io>
2015-01-21 13:14:19 -0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var gm = require('gm');
 
 module.exports = function(source, opts, cb) {
+  if('function' === typeof opts) {
+    cb = opts;
+    opts = {};
+  }
+
   gm(source)
     .toBuffer('png', cb);
 };
